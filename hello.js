ee62a2791e3b691642b46f9e4090c4e5bbcda293
kirbybowwow <kirbybowwow@gmail.com>
2022-08-05 11:21:18 -0700

    Here are my files

--- hello.js
+++ hello.js
@@ -1,3 +1,7 @@
 function hello() {
-  return "Hello World!";
+  return "This will be a passing test!";
+}
+
+function sum() {
+  return 2 + 3;
 }
--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,11 @@
 var utils  = require('course-utilities');
 var hello = utils.load('./hello.js', 'hello');
+var sum = utils.load('./hello.js', 'sum')
 
 test('outputs the correct string', () => {
-  expect(hello()).toBe("Hello World!");
+  expect(hello()).toBe("This will be a passing test!");
+});
+
+test('outputs the correct number', () => {
+  expect(sum()).toBe(5);
 });
